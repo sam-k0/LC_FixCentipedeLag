7c5ca910d906df6111d5dc4da4d2f5269dacf59e
sam-k0 <56673835+sam-k0@users.noreply.github.com>
2023-12-04 18:32:27 +0100

    Fixes #1

--- Plugin.cs
+++ Plugin.cs
@@ -2,18 +2,23 @@
 using BepInEx.Logging;
 using HarmonyLib;
 using UnityEngine;
+using System.Collections.Generic;
+
 
 namespace LC_Optim
 {
-    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInPlugin(PluginMetadata.PLUGIN_GUID, PluginMetadata.PLUGIN_NAME, PluginMetadata.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
         private Harmony thisHarmony;
+        private static Dictionary<int, ulong> instanceMap = new Dictionary<int, ulong>();
+        private static ulong deadtimer = 100;
         private void Awake()
         {
-            thisHarmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
-            thisHarmony.Patch(typeof(CentipedeAI).GetMethod("DoAIInterval"), postfix: new HarmonyMethod(typeof(Plugin), nameof(RemoveLagCentipede)));
+            thisHarmony = new Harmony(PluginMetadata.PLUGIN_GUID);
+            thisHarmony.Patch(typeof(CentipedeAI).GetMethod("DoAIInterval"), postfix: new HarmonyMethod(typeof(Plugin), nameof(RemoveLagCentipede)), prefix: new HarmonyMethod(typeof(Plugin), nameof(RemoveLagCentipede)));
             Logger.LogWarning("Registered the patch method.");
+            Logger.LogWarning("Deactivating UnityLog");
         }
 
 
@@ -21,8 +26,30 @@ namespace LC_Optim
         {
             if(!__instance.TargetClosestPlayer(1.5f, false, 70f))
             {
-                // Enemy is probably stuck
-                __instance.KillEnemyOnOwnerClient();                
+                int oid = __instance.GetInstanceID();
+                ulong tfc = (ulong)Time.frameCount;
+                if (!instanceMap.ContainsKey(oid))
+                { 
+                    instanceMap.Add(oid, tfc);
+                }
+                else
+                {
+                    // already tracked
+                    // check when the last call was
+                    ulong lastFrameCount = instanceMap[oid];
+                    ulong framesPassed = tfc - lastFrameCount;
+                    if(framesPassed <= deadtimer)
+                    {
+                        // Delete enemy as it triggered this too often
+                        __instance.KillEnemy(true);
+                        // clear entry
+                        instanceMap.Remove(oid);
+                    }
+                    else // update the entry
+                    {
+                        instanceMap[oid] = tfc;
+                    }
+                }               
             }
         }
 
---
+++ PluginMetadata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LC_Optim
+{
+    class PluginMetadata
+    {
+        public const string PLUGIN_GUID = "mnc.fixcentipedelag";
+        public const string PLUGIN_NAME = "FixCentipedeLag";
+        public const string PLUGIN_VERSION = "2023.12.4";
+
+    }
+}
